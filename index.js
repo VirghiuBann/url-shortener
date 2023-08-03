39236ab6af437a17fe96621263b62c80fe8ea5cd
virghiu <virghiu@gmail.com>
2023-08-03 17:36:41 -0400

    post and get url shortener

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const dns = require('dns').promises;
+
+const urlShortMemo = [];
+
 const app = express();
 
 // Basic Configuration
@@ -16,11 +20,44 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/shorturl', function (req, res) {
-  
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', async (req, res) => {
+  const url = req.body.url; 
+
+  try {
+    const urlValid = await validateUrl(url);    
+    const urlObj = { original_url: urlValid.origin, short_url: urlShortMemo.length + 1 }
+    urlShortMemo.push(urlObj);
+    res.json(urlObj);
+  } catch (error) {
+    res.json({ error: 'invalid url' });
+  }
+
 });
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = parseInt(req.params.short_url);
+
+  const urlObj = urlShortMemo.find((item) => item.short_url === shortUrl);
+
+  if (!urlObj) {
+    return res.json({ error: 'invalid url' });
+  }
+  res.redirect(urlObj.original_url);
+
+});
+
+// check if is valid url
+const validateUrl = async(url) => {
+  const urlObj = new URL(url);
+  const hostname = urlObj.hostname;
+  try {
+    await dns.lookup(hostname);
+    return { origin: urlObj.origin };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+ 
+}
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
